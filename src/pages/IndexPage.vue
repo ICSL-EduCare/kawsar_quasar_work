e789dd89ef0da4d7c4489d520f2d19bcdb862d3e
Kawsar <kawsar@gmail.com>
2026-09-14 15:12:24 +0600

    Column wrapping

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,31 +1,22 @@
 <template>
-<div class="q-pa-md example-row-vertical-alignment">
-  <q-badge>items-start</q-badge>
-  <div class="row items-start">
-    <div class="col"> One of three cols </div>
-    <div class="col"> One of three cols </div>
-    <div class="col"> One of three cols </div>
-  </div>
-
-  <q-badge>items-center</q-badge>
-  <div class="row items-center">
-    <div class="col"> One of three cols </div>
-    <div class="col"> One of three cols </div>
-    <div class="col"> One of three cols </div>
-  </div>
-
-  <q-badge>items-end</q-badge>
-  <div class="row items-end">
-    <div class="col"> One of three cols </div>
-    <div class="col"> One of three cols </div>
-    <div class="col"> One of three cols </div>
-  </div>
-
-  <q-badge>self-*</q-badge>
+<div class="q-pa-md example-row-column-wrapping">
   <div class="row">
-    <div class="col self-start"> .self-start </div>
-    <div class="col self-center"> .self-center </div>
-    <div class="col self-end"> .self-end </div>
+    <div class="col-9">.col-9</div>
+    <div class="col-4"
+      >.col-4<br />Since 9 + 4 = 13 &gt; 12, this 4-column-wide div gets
+      wrapped onto a new line as one contiguous unit.</div
+    >
+    <div class="col-5"
+      >.col-5<br />Subsequent columns continue along the new line.</div
+    >
+  </div>
+
+  <div class="row">
+    <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+    <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+
+    <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+    <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
   </div>
 </div>
 </template>
