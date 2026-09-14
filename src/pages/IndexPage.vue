4503569fdbf9f5cfd338bd6309803c973224ee3a
Kawsar <kawsar@gmail.com>
2026-09-14 14:28:10 +0600

    wrapping

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,18 @@
 <template>
 <div class="row">
-  <div class="col-8">two thirds</div>
-  <div class="col-2">one sixth</div>
-  <div class="col-auto">auto size based on content and available space</div>
-  <div class="col">fills remaining available space</div>
+  <div class="col-2">...</div>
+
+  <!-- 2 + 6 < 12, so next element is placed on same line -->
+  <div class="col-6">...</div>
+
+  <!-- 2 + 6 + 10 > 12, so next element wraps to next line -->
+  <div class="col-10">...</div>
+
+  <!--
+    10 + 3 > 12, so next element wraps to next line.
+    Note that we take into consideration the current line only
+    (with col-10 only, since it was wrapped to its own line).
+  -->
+  <div class="col-3">...</div>
 </div>
 </template>
