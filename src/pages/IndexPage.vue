053271a7f903edc5a10124e4bc1bd67b76e7da0e
Kawsar <kawsar@gmail.com>
2026-09-14 15:34:37 +0600

    Pros, cons and how to workaround problems - “q-gutter-{size}” vs. “q-col-gutter-{size}”

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,28 +1,43 @@
 <template>
 <div class="q-pa-md" style="max-width: 500px">
-  <p>q-gutter-xs</p>
-  <div class="q-gutter-xs">
-    <q-btn color="brown" label="Button" v-for="n in 7" :key="`xs-${n}`" />
-  </div>
-
-  <p class="q-mt-md">q-gutter-sm</p>
-  <div class="q-gutter-sm">
-    <q-btn color="teal" label="Button" v-for="n in 7" :key="`sm-${n}`" />
-  </div>
-
-  <p class="q-mt-md">q-gutter-md</p>
-  <div class="q-gutter-md">
-    <q-btn color="accent" label="Button" v-for="n in 7" :key="`md-${n}`" />
-  </div>
-
-  <p class="q-mt-md">q-gutter-lg</p>
-  <div class="q-gutter-lg">
-    <q-btn color="red" label="Button" v-for="n in 7" :key="`lg-${n}`" />
-  </div>
-
-  <p class="q-mt-md">q-gutter-xl</p>
-  <div class="q-gutter-xl">
-    <q-btn color="indigo" label="Button" v-for="n in 7" :key="`xl-${n}`" />
+  <div class="row">
+    <div class="offset-1 col-3 column justify-between">
+      <p>Styling on parent</p>
+      <div class="text-dark">
+        <div class="bg-amber q-pa-sm">Amber block</div>
+        <div class="row q-gutter-lg bg-blue-4">
+          <div class="q-pa-md bg-blue-10 text-white" v-for="n in 4" :key="n"
+            >C</div
+          >
+        </div>
+      </div>
+    </div>
+    <div class="offset-1 col-3 column justify-between">
+      <p>Styling on wrapper - .row</p>
+      <div class="text-dark">
+        <div class="bg-amber q-pa-sm">Amber block</div>
+        <div class="bg-blue-4 row">
+          <div class="row q-gutter-lg">
+            <div class="q-pa-md bg-blue-10 text-white" v-for="n in 4" :key="n"
+              >C</div
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="offset-1 col-3 column justify-between">
+      <p>Styling on wrapper - .overflow-auto</p>
+      <div class="text-dark">
+        <div class="bg-amber q-pa-sm">Amber block</div>
+        <div class="bg-blue-4 overflow-auto">
+          <div class="row q-gutter-lg">
+            <div class="q-pa-md bg-blue-10 text-white" v-for="n in 4" :key="n"
+              >C</div
+            >
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 </template>
