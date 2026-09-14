b64ec4b99c2c041daa7d8a86a7558de36477f808
Kawsar <kawsar@gmail.com>
2026-09-14 15:15:01 +0600

    Offsetting columns

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,22 +1,17 @@
 <template>
-<div class="q-pa-md example-row-column-wrapping">
+<div class="q-pa-md example-row-offsetting-columns">
   <div class="row">
-    <div class="col-9">.col-9</div>
-    <div class="col-4"
-      >.col-4<br />Since 9 + 4 = 13 &gt; 12, this 4-column-wide div gets
-      wrapped onto a new line as one contiguous unit.</div
-    >
-    <div class="col-5"
-      >.col-5<br />Subsequent columns continue along the new line.</div
-    >
+    <div class="col-md-4">.col-md-4</div>
+    <div class="col-md-4 offset-md-4">.col-md-4 .offset-md-4</div>
   </div>
 
   <div class="row">
-    <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
-    <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+    <div class="col-md-3 offset-md-3">.col-md-3 .offset-md-3</div>
+    <div class="col-md-3 offset-md-3">.col-md-3 .offset-md-3</div>
+  </div>
 
-    <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
-    <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+  <div class="row">
+    <div class="col-md-6 offset-md-3">.col-md-6 .offset-md-3</div>
   </div>
 </div>
 </template>
