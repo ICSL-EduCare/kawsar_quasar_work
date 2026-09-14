5e5de585da4ca6af14b5efcc521c8833e1e4063c
Kawsar <kawsar@gmail.com>
2026-09-14 15:07:14 +0600

    all breakpoint

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,18 +1,15 @@
 <template>
-<div class="row">
-  <div class="col-2">...</div>
+<div class="q-pa-md example-row-all-breakpoints">
+  <div class="row">
+    <div class="col">.col</div>
+    <div class="col">.col</div>
+    <div class="col">.col</div>
+    <div class="col">.col</div>
+  </div>
 
-  <!-- 2 + 6 < 12, so next element is placed on same line -->
-  <div class="col-6">...</div>
-
-  <!-- 2 + 6 + 10 > 12, so next element wraps to next line -->
-  <div class="col-10">...</div>
-
-  <!--
-    10 + 3 > 12, so next element wraps to next line.
-    Note that we take into consideration the current line only
-    (with col-10 only, since it was wrapped to its own line).
-  -->
-  <div class="col-3">...</div>
+  <div class="row">
+    <div class="col-8">.col-8</div>
+    <div class="col-4">.col-4</div>
+  </div>
 </div>
 </template>
