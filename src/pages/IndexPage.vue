2cc5860e23e196d47b2a3725d9df30cb73bba785
Kawsar <kawsar@gmail.com>
2026-09-14 15:09:45 +0600

    Stacked to horizontal

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,14 @@
 <template>
-<div class="q-pa-md example-row-all-breakpoints">
+<div class="q-pa-md example-row-stacked-to-horizontal">
   <div class="row">
-    <div class="col">.col</div>
-    <div class="col">.col</div>
-    <div class="col">.col</div>
-    <div class="col">.col</div>
+    <div class="col-12 col-md-8">.col-12 .col-md-8</div>
+    <div class="col-12 col-md-4">.col-12 .col-md-4</div>
   </div>
 
   <div class="row">
-    <div class="col-8">.col-8</div>
-    <div class="col-4">.col-4</div>
+    <div class="col-12 col-md">.col-12 .col-md</div>
+    <div class="col-12 col-md">.col-12 .col-md</div>
+    <div class="col-12 col-md">.col-12 .col-md</div>
   </div>
 </div>
 </template>
